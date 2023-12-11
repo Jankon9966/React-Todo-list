379cd90db30cf58457df1ed173fb6900c3cd6255
Janko Novakovic <licardian9996@gmail.com>
2023-12-11 22:59:29 +0100

    initial commit

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 
 const TodoItem = (props) => {
+  
+  const deleteTaskItemHandler = ({ id }) => {
+    props.setEnteredText(props.enteredText.filter((todo) => todo.id !== id));
+  };
+
   return (
     <div className="todo_item_container">
       {props.enteredText.map((todo) => {
         return (
           <div key={todo.id} className="todo_list">
             <h4>{todo.text}</h4>
+            <span onClick={() => deleteTaskItemHandler(todo)}>x</span>
           </div>
         );
       })}
